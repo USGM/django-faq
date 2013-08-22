fb7ba075acf1d146e8edfed552f1c742397d0a3e
Jonathan Piacenti <jpiacenti@usglobalmail.com>
2013-08-22 12:50:09 -0500

    Bugfix to the admin. Status still lingered (still)

--- faq/admin.py
+++ faq/admin.py
@@ -3,7 +3,7 @@ from models import Question, Topic
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ['text', 'sort_order']
-    list_editable = ['sort_order', 'status']
+    list_editable = ['sort_order']
         
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Topic, admin.ModelAdmin)
