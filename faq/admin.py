ff0f9dad467a3e1b34de66b30fbd96bbd15b22ee
Jonathan Piacenti <jpiacenti@usglobalmail.com>
2013-08-22 12:45:35 -0500

    Updated admin to reflect this change.

--- faq/admin.py
+++ faq/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from models import Question, Topic
-            
-class TopicAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
-    
+
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ['text', 'sort_order', 'status']
     list_editable = ['sort_order', 'status']
         
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Topic, TopicAdmin)
+admin.site.register(Topic, admin.ModelAdmin)
