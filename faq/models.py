75dc8a755b89c5643289d03749d26bf96108d2ab
Jonathan Piacenti <jpiacenti@usglobalmail.com>
2013-08-22 11:27:12 -0500

    Basic Custom User model fix

--- faq/models.py
+++ faq/models.py
@@ -1,9 +1,14 @@
 import datetime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 from managers import QuestionManager
+
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+except ImportError:
+    from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """
