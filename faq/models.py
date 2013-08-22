1815634ce8dd0b49d41c4a7d923f4fd1de2bc8c2
Jonathan Piacenti <jpiacenti@usglobalmail.com>
2013-08-22 12:47:30 -0500

    Apparently missed pushing the models in a previous commit. Whoops.

--- faq/models.py
+++ faq/models.py
@@ -1,8 +1,6 @@
 import datetime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.template.defaultfilters import slugify
-from managers import QuestionManager
 
 try:
     from django.contrib.auth import get_user_model
@@ -15,7 +13,6 @@ class Topic(models.Model):
     Generic Topics for FAQ question grouping
     """
     name = models.CharField(_('name'), max_length=150)
-    slug = models.SlugField(_('slug'), max_length=150)
     sort_order = models.IntegerField(_('sort order'), default=0,
         help_text=_('The order you would like the topic to be displayed.'))
 
@@ -31,24 +28,9 @@ class Topic(models.Model):
         return self.name
 
 class Question(models.Model):
-    HEADER = 2
-    ACTIVE = 1
-    INACTIVE = 0
-    STATUS_CHOICES = (
-        (ACTIVE,    _('Active')),
-        (INACTIVE,  _('Inactive')),
-        (HEADER,    _('Group Header')),
-    )
-    
     text = models.TextField(_('question'), help_text=_('The actual question itself.'))
     answer = models.TextField(_('answer'), blank=True, help_text=_('The answer text.'))
     topic = models.ForeignKey(Topic, verbose_name=_('topic'), related_name='questions')
-    slug = models.SlugField(_('slug'), max_length=100)
-    status = models.IntegerField(_('status'),
-        choices=STATUS_CHOICES, default=INACTIVE, 
-        help_text=_("Only questions with their status set to 'Active' will be "
-                    "displayed. Questions marked as 'Group Header' are treated "
-                    "as such by views and templates that are set up to use them."))
     
     protected = models.BooleanField(_('is protected'), default=False,
         help_text=_("Set true if this question is only visible by authenticated users."))
@@ -56,8 +38,6 @@ class Question(models.Model):
     sort_order = models.IntegerField(_('sort order'), default=0,
         help_text=_('The order you would like the question to be displayed.'))
 
-    objects = QuestionManager()
-    
     class Meta:
         verbose_name = _("Frequent asked question")
         verbose_name_plural = _("Frequently asked questions")
@@ -65,27 +45,3 @@ class Question(models.Model):
 
     def __unicode__(self):
         return self.text
-
-    def save(self, *args, **kwargs):
-        # Set the date updated.
-        self.updated_on = datetime.datetime.now()
-        
-        # Create a unique slug, if needed.
-        if not self.slug:
-            suffix = 0
-            potential = base = slugify(self.text[:90])
-            while not self.slug:
-                if suffix:
-                    potential = "%s-%s" % (base, suffix)
-                if not Question.objects.filter(slug=potential).exists():
-                    self.slug = potential
-                # We hit a conflicting slug; increment the suffix and try again.
-                suffix += 1
-        
-        super(Question, self).save(*args, **kwargs)
-
-    def is_header(self):
-        return self.status == Question.HEADER
-
-    def is_active(self):
-        return self.status == Question.ACTIVE
